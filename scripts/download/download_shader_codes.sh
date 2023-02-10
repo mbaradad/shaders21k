ef813b707ca66d847eb8010aed6674a5db11db86
Manel Baradad Jurjo <manelbaradad@gmail.com>
2023-02-10 12:46:13 -0500

    Update download_shader_codes.sh

--- scripts/download/download_shader_codes.sh
+++ scripts/download/download_shader_codes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p shader_codes
 
-wget -O shader_codes/all_codes.zip http://data.csail.mit.edu/synthetic_training/shaders21k/all_codes.zip
+wget -O all_codes.zip http://data.csail.mit.edu/synthetic_training/shaders21k/all_codes.zip
 unzip shader_codes/all_codes.zip
-rm shader_codes/all_codes.zip
+rm all_codes.zip
