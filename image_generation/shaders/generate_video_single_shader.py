1ddd6f26c73de94b5ea1988ff0b42df59079fb84
Manel Baradad Jurjo <mbaradad@mit.edu>
2024-02-14 09:51:01 -0500

    Added code to generate video from a single shader, continuously

---
+++ image_generation/shaders/generate_video_single_shader.py
@@ -0,0 +1,44 @@
+from utils import *
+
+from image_generation.shaders.glsl_utils import *
+from image_generation.shaders.renderer_moderngl import RendererModernGL, get_program_from_shader_path
+
+def generate_video_single_shader(shader_path, output_path, duration, resolution, fps, gpu):
+  assert os.path.exists(shader_file), "Fragment path {} does not exist!"
+
+  os.makedirs(output_path, exist_ok=True)
+
+  program = get_program_from_shader_path(shader_path)
+  total_samples = duration * fps
+  renderer = RendererModernGL([program], 
+                              n_images_to_generate=total_samples, 
+                              fps=fps,
+                              resolution=resolution,
+                              temporal_sampling='continuous', 
+                              gpu=gpu,
+                              max_failed_samples=0)
+
+  imgs = renderer.render()
+
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  # default args for small scale with alexnet, same as training with noise paper
+
+  parser.add_argument("--shader-file", type=str, default='/data/vision/torralba/movies_sfm/releases/shaders21k/shader_codes/shadertoy/l/ltVfWG.fragment')
+  parser.add_argument("--duration", type=int, default=20, help="Duration of the video in seconds")
+  parser.add_argument("--resolution", type=int, default=512)
+  parser.add_argument("--fps", type=int, default=24)
+  parser.add_argument("--output-path", type=str, default='/tmp/ltVfWG.mp4')
+  parser.add_argument("--gpu", type=int, default=0)
+  
+
+  args = parser.parse_args()
+
+  generate_video_single_shader(args.shader_file,
+                               args.output_path,
+                               args.duration,
+                               args.resolution,
+                               args.fps,
+                               args.gpu)
