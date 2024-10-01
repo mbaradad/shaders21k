8809464ce74bc46c5828656d6683c6dde02c6cd2
Manel Baradad Jurjo <manelbaradad@gmail.com>
2024-10-01 11:42:30 +0200

    Update download_shader_codes.sh

--- scripts/download/download_shader_codes.sh
+++ scripts/download/download_shader_codes.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "This script does not work anymore, as codes have been put offline"
 mkdir -p shader_codes
 
 wget -O all_codes.zip http://data.csail.mit.edu/synthetic_training/shaders21k/all_codes.zip
